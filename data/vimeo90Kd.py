17b2d79ab8a2be8b1670eb3fda2a3f14c7609ccd
Hubert482 <72866544+Hubert482@users.noreply.github.com>
2021-03-27 14:23:32 +0100

    Add files via upload

--- data/vimeo90Kd.py
+++ data/vimeo90Kd.py
@@ -24,7 +24,7 @@ class VimeoTriplet(Dataset):
             transforms.RandomCrop(256),
             transforms.RandomHorizontalFlip(0.5),
             transforms.RandomVerticalFlip(0.5),
-            transforms.ColorJitter(0.05, 0.05, 0.05, 0.05),
+            #transforms.ColorJitter(0.05, 0.05, 0.05, 0.05),
             transforms.ToTensor()
         ])
         
@@ -43,28 +43,27 @@ class VimeoTriplet(Dataset):
         img1d = cv2.imread(imgpaths[0][:-4]+"_depth.png", cv2.IMREAD_GRAYSCALE)
         img2d = cv2.imread(imgpaths[1][:-4]+"_depth.png", cv2.IMREAD_GRAYSCALE)
         img3d = cv2.imread(imgpaths[2][:-4]+"_depth.png", cv2.IMREAD_GRAYSCALE)
-        b_channel1, g_channel1, r_channel1 = cv2.split(img1t)
-        b_channel2, g_channel2, r_channel2 = cv2.split(img2t)
-        b_channel3, g_channel3, r_channel3 = cv2.split(img3t)
-        img1d= cv2.cvtColor(img1d,cv2.COLOR_GRAY2RGB)
-        img2d= cv2.cvtColor(img2d,cv2.COLOR_GRAY2RGB)
-        img3d= cv2.cvtColor(img3d,cv2.COLOR_GRAY2RGB)
-        d_channel1, d_channel1, d_channel1 = cv2.split(img1d)
-        d_channel2, d_channel2, d_channel2 = cv2.split(img2d)
-        d_channel3, d_channel3, d_channel3 = cv2.split(img3d)
 
-        depth1 = np.ones(d_channel2.shape, dtype=d_channel1.dtype)
-        depth2 = np.ones(d_channel1.shape, dtype=d_channel2.dtype)
-        depth3 = np.ones(d_channel3.shape, dtype=d_channel3.dtype)
-        img1 = cv2.merge((b_channel1, g_channel1, r_channel1, depth1), cv2.COLOR_BGRA2RGBA)
-        img2 = cv2.merge((b_channel2, g_channel2, r_channel2, depth2), cv2.COLOR_BGRA2RGBA)
-        img3 = cv2.merge((b_channel3, g_channel3, r_channel3, depth3), cv2.COLOR_BGRA2RGBA)
+        img1b, img1g, img1r = cv2.split(img1t)
+        img2b, img2g, img2r = cv2.split(img2t)
+        img3b, img3g, img3r = cv2.split(img3t)
+        #imd = cv2.merge([depth , depth , depth ])
+
+        img1 = cv2.merge([img1b, img1g, img1r, img1d], cv2.COLOR_BGRA2RGBA)
+        img2 = cv2.merge((img2b, img2g, img2r, img2d), cv2.COLOR_BGRA2RGBA)
+        img3 = cv2.merge((img3b, img3g, img3r, img3d), cv2.COLOR_BGRA2RGBA)
         #img1 = cv2.cvtColor(img1, cv2.COLOR_BGRA2RGBA)
         #cv2.imwrite("1.png", img1)
+        #us, us, us, depth = cv2.split(img1)
+        #imd = cv2.merge([depth , depth , depth ])
+        #cv2.imwrite(f"depth.png", img1)
         #cv2.waitKey(0)
         img1 = Image.fromarray(img1)
         img2 = Image.fromarray(img2)
         img3 = Image.fromarray(img3)
+        #image_without_alpha = img[:,:,:3]
+        #img = cv2.cvtColor(image_without_alpha, cv2.COLOR_RGBA2BGRA)
+
         # Data augmentation
         if self.training:
             seed = random.randint(0, 2**32)
